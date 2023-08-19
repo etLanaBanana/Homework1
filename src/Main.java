d0206a84730132bbe063e2888f1009cecb2c098b
etLanaBanana <svetlanaskovoroda@gmail.ru>
2023-08-19 22:15:31 +0300

    calculation of body mass index

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
+//Подсчитать индекс массы тела
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        int bmi;
+        Scanner scanner = new Scanner(System.in);
+        int weight = scanner.nextInt();
+        int height = scanner.nextInt();
+        bmi = weight/height^2;
+        if (bmi <= 18.5)
+            System.out.println("недовес");
+        else if (bmi <= 25)
+            System.out.println("вес в норме");
+        else if (bmi <= 30)
+            System.out.println("лишний вес");
+        else  if (bmi > 30)
+            System.out.println("ожирение");
     }
 }
